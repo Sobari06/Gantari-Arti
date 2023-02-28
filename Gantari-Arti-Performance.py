4036952eb4e8e65fa0715631723ebe98f80fe631
Sobari06 <126376471+Sobari06@users.noreply.github.com>
2023-02-28 17:26:06 +0700

    Update Gantari-Arti-Performance.py

--- Gantari-Arti-Performance.py
+++ Gantari-Arti-Performance.py
@@ -206,7 +206,7 @@ st.write(f'Month: {selected_month}')
 df_filtered = dfZ[dfZ['DATE_1'] == selected_month]
 if len(df_filtered) > 0:
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(8, 10))
-    sns.boxplot(x='Biro dan Departemen', y='Performa', data=df_filtered, ax=ax)
+    sns.boxplot(x='Biro dan Departemen', y='Performa', data=df_filtered, ax=ax1)
     ax.set_title(f'Boxplot Performa Kerja Seluruh Biro dan Departemen ({selected_month})')
     
     # Rotate x-labels for better visibility
